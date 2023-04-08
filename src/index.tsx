aae0647f8fd0deb0a83edd2317ccffc5b68120b8
Igor Shargin <igor20513@gmail.com>
2023-04-08 23:40:39 +0500

    feat(firebase.ts): add firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 
+import { getDatabase, onValue, ref } from 'firebase/database'
 import ReactDOM from 'react-dom/client'
-
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
+import './firebase'
 import { App } from './App'
+
+const db = getDatabase()
+const starCountRef = ref(db, 'products')
+
+onValue(starCountRef, snapshot => {
+  const data = snapshot.val()
+
+  console.log(data)
+})
+console.log(getDatabase())
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
