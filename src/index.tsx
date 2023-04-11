254a507ed9b496c032c679b5a4ce6d058a5555af
Igor Shargin <igor20513@gmail.com>
2023-04-11 12:02:33 +0500

    fix(index.tsx): added Provider, deleted firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,19 @@
 import React from 'react'
 
-import { getDatabase, onValue, ref } from 'firebase/database'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
-import './firebase'
 import { App } from './app/App'
-
-const db = getDatabase()
-const starCountRef = ref(db, 'products')
-
-onValue(starCountRef, snapshot => {
-  const data = snapshot.val()
-
-  console.log(data)
-})
-console.log(getDatabase())
+import { store } from './app/store'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 )
