88bed5db5344cfc56737861a98d7c81b62ed84c0
Igor Shargin <igor20513@gmail.com>
2023-04-11 12:01:14 +0500

    feat: created store

---
+++ src/app/store.ts
@@ -0,0 +1,19 @@
+import { AnyAction, combineReducers, configureStore, ThunkAction } from '@reduxjs/toolkit'
+import thunk from 'redux-thunk'
+
+import { productReducer } from '../features/Main/main-slice'
+
+const rootReducer = combineReducers({
+  main: productReducer,
+})
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(thunk),
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, AnyAction>
+
+//@ts-ignore
+window.store = store
